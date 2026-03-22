5745d62cf22ba87cd0740b48a942d643346758d5
naveen <185893220+codamee@users.noreply.github.com>
2026-03-22 22:19:18 +0530

    feat:add nav component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-      <div className='flex justify-between'>
-          <div>
-              <p>Creators</p>
-              <p>Features</p>
-              <p>Pricing</p>
-              <p>Resources</p>
-              <button>Updates</button>
-          </div>
-          <div>PATREON</div>
-          <div>
-              <div>
-                  <input type="search" name="" id="" />
-              </div>
-              <button>Log in</button>
-              <button>Get Started</button>
-          </div>
-    </div>
-  )
+    return (
+        <div className='flex justify-between items-center p-15 bg-black text-white cursor-pointer'>
+            <div className='flex items-center'>
+                <p className='hover:bg-white rounded-4xl px-6 py-3 hover:text-black'>Creators</p>
+                <p className='hover:bg-white rounded-4xl px-6 py-3 hover:text-black'>Features</p>
+                <p className='hover:bg-white rounded-4xl px-6 py-3 hover:text-black'>Pricing</p>
+                <p className='hover:bg-white rounded-4xl px-6 py-3 hover:text-black'>Resources</p>
+                <button className='px-6 py-3 mx-4 border rounded-4xl cursor-pointer hover:text-black hover:bg-white'>Updates</button>
+            </div>
+            <div className='w-1/5 text-2xl font-bold '>PATREON</div>
+            <div className='flex gap-4 items-center'>
+                <div>
+                    <input className='px-6 py-3 border rounded-4xl  focus:text-black focus:bg-white ' type="search" name="" id="" placeholder='find a creator' />
+                </div>
+                <button className='px-6 py-3 border rounded-4xl cursor-pointer hover:text-black hover:bg-white'>Log in</button>
+                <button className='px-6 py-3 bg-white text-black cursor-pointer rounded-4xl'>Get Started</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar

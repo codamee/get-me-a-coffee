293be5291ea20c97250ea69dd68f156c2497f30f
naveen <185893220+codamee@users.noreply.github.com>
2026-03-24 08:48:23 +0530

    feat : add the dashboard sidebar

--- app/(dash)/dashboard/page.js
+++ app/(dash)/dashboard/page.js
@@ -1,8 +1,48 @@
+"use client"
 import React from 'react'
-
+import Link from 'next/link'
+import { UserButton, Show } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs'
 const Dashboard = () => {
+  const { isLoaded, isSignedIn, user } = useUser();
   return (
-    <div>Dashboard</div>
+    <div className='h-screen flex'>
+      <div className="sidebar h-full flex flex-col justify-between items-center p-5 border-r border-r-gray-200 ">
+        <div className='flex flex-col gap-4 font-bold text-slate-700'>
+          <div className='flex items-end justify-center'>
+            <img src="/logomark-animated.webp" alt="logo" width={40} />
+            {/* <p className='font-bold'>ATREON</p> */}
+          </div>
+          <div className='flex gap-2 px-4 py-2 rounded-xl hover:bg-mauve-100'>
+            <img src="/svg/home.svg" alt="Home" width={20} />
+            <Link href={"/"}><p>Home</p></Link>
+          </div>
+          <div className='flex gap-2 px-4 py-2 rounded-xl hover:bg-mauve-100 cursor-pointer'>
+            <img src="/svg/dash.svg" alt="Dashboard" width={20} />
+            <p>Dashboard</p>
+          </div>
+        </div>
+        <div className="login flex justify-center  items-center border px-4 py-2 rounded-4xl hover:bg-mauve-100">
+          <Show when="signed-in">
+            <UserButton
+              showName={true}
+              appearance={{
+                elements: {
+                  userButtonBox: {
+                    flexDirection: "row-reverse",
+                  },
+                  userButtonOuterIdentifier: {
+                    fontWeight: "700",  
+                    color: "black",       
+                  }
+                }
+              }}
+            />
+          </Show>
+        </div>
+      </div>
+      <div className="main"></div>
+    </div>
   )
 }
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -20,6 +20,7 @@ const Navbar = () => {
                 <div>
                     <input className='px-6 py-3 border rounded-4xl  focus:text-black focus:bg-white' type="search" name="" id="" placeholder='find a creator' />
                 </div>
+
                 <Show when="signed-out">
                     <SignInButton >
                         <button className='px-6 py-3 border rounded-4xl cursor-pointer hover:text-black hover:bg-white'>
@@ -30,9 +31,40 @@ const Navbar = () => {
                         <button className='px-6 py-3 bg-white text-black cursor-pointer rounded-4xl'>Get Started</button>
                     </SignUpButton>
                 </Show>
+
                 <Show when="signed-in">
-                    <UserButton />
+                    <Link href={"/dashboard"}>
+                        <button className='px-6 py-3 bg-white text-black cursor-pointer rounded-4xl'>Dashboard</button>
+                    </Link>
                 </Show>
+
+                <div className='flex rounded-4xl border px-4 py-3 cursor-pointer'>
+                    <Show when="signed-in">
+                        <UserButton
+                            showName={true}
+                            appearance={{
+                                elements: {
+                                    // 1. Increase the size of the trigger and the image box
+
+
+
+                                    userButtonBox: {
+                                        flexDirection: "row-reverse",
+                                    },
+
+                                    userButtonOuterIdentifier: {
+                                        // fontSize: "1.1rem",
+                                        fontWeight: "700",
+                                        color: "white",
+
+                                    }
+                                }
+                            }}
+                        />
+                    </Show>
+                </div>
+
+
             </div>
         </nav>
     )

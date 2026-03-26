bf9a4375c024f7d35b121724945d946fcc7631ed
naveen <185893220+codamee@users.noreply.github.com>
2026-03-26 10:17:02 +0530

    feat : add the Dashboard ui

--- app/(dash)/dashboard/page.js
+++ app/(dash)/dashboard/page.js
@@ -6,7 +6,7 @@ import { useUser } from '@clerk/nextjs'
 const Dashboard = () => {
   const { isLoaded, isSignedIn, user } = useUser();
   return (
-    <div className='h-screen flex '>
+    <div className='h-dvh flex overflow-hidden   '>
       <div className="sidebar h-full flex flex-col justify-between items-center px-10 py-5 border-r border-r-gray-200 ">
         <div className='flex flex-col gap-4 font-bold text-slate-700'>
           <div className='flex items-end justify-center'>
@@ -42,20 +42,46 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="main h-full flex-1">
-        <div className='w-full bg-red-300 relative'>
-          <img src="/dashBg.jpg" alt="dash" className="w-full h-75 object-cover" />
-          <div className=' flex flex-col gap-4 items-center absolute bottom-[-170] right-140'>
-            <img src="/dashprofile.jpg" alt="profile" className='w-30 rounded-2xl border border-gray-700' />
+        <div className='w-full flex flex-col gap-2'>
+          <img src="/dashBg.jpg" alt="dash" className="w-full h-50 object-cover" />
+          <div className='w-full flex flex-col gap-2 items-center justify-center'>
+            <img src="/dashprofile.jpg" alt="profile" className='w-30 rounded-[10rem] border border-gray-700' />
             <div className='flex flex-col items-center gap-2'>
               <p className='text-3xl font-bold'>Raw Coding</p>
               <p>creating Web Development Tutorials</p>
               <p className='text-gray-400 text-sm'>419 members . 1 post . $14525 raised</p>
             </div>
+            <div className='flex justify-around items-center p-4 gap-4 w-full'>
+              <div className='p-4 rounded-md w-full flex flex-col gap-4 h-78 bg-slate-200 '>
+                <p className='font-bold text-xl'>Supporters</p>
+                <div className='flex flex-col gap-3 h-[90%] overflow-auto'>
+
+                  <p className='px-2 py-1 bg-slate-300 rounded-md'>Shubam donated $500 : "Lots of love bro "</p>
+                  <p className='px-2 py-1 bg-slate-300 rounded-md'>Shubam donated $500 : "Lots of love bro "</p>
+                  <p className='px-2 py-1 bg-slate-300 rounded-md'>Shubam donated $500 : "Lots of love bro "</p>
+
+                </div>
+              </div>
+              <div className='bg-slate-200 p-4 rounded-md w-full flex flex-col gap-4 h-78'>
+                <p className='font-bold text-xl'>Make a Payment</p>
+                <div className='flex flex-col gap-2'>
+                  <input className='px-4 py-2 w-full rounded-md outline-0  bg-slate-300' type="text" name="name" id="name" placeholder='Enter Name' />
+                  <input className='px-4 py-2 w-full rounded-md outline-0  bg-slate-300' type="text" name="message" id="message" placeholder='Enter Message' />
+                  <input className='px-4 py-2 w-full rounded-md outline-0  bg-slate-300' type="number" name="amount" id="amount" placeholder='Enter Amount' />
+                  <button className='px-4 py-2 font-semibold text-white bg-black rounded-md'>Submit</button>
+                </div>
+                <div className='flex gap-4'>
+                  <p className='bg-black text-white font-semibold px-2 py-1 rounded-md'>Pay $10</p>
+                  <p className='bg-black text-white font-semibold px-2 py-1 rounded-md'>Pay $50</p>
+                  <p className='bg-black text-white font-semibold px-2 py-1 rounded-md'>Pay $100</p>
+                </div>
+              </div>
+            </div>
           </div>
 
         </div>
-        <div></div>
       </div>
+
     </div>
   )
 }
